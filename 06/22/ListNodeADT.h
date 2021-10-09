29a93121dc05d995b0dad22b41cdf1269cd8e903
NoahCardoza <noahcardoza@gmail.com>
2021-10-08 20:43:36 -0700

    start 6.22

---
+++ 06/22/ListNodeADT.h
@@ -0,0 +1,29 @@
+// Specification file for the ListNode class
+// IDE:
+// Written By:
+#ifndef LISTNODE_H
+#define LISTNODE_H
+#include <iostream>
+//#include "College.h"
+// ^^^  not included here anymore
+
+template <class T>
+class ListNode
+{
+private:
+    T data;      // store data
+    ListNode *forw;    // a pointer to the next node in the list
+    ListNode *back;    // a pointer to the previous node in the list
+public:
+    //Constructor
+    ListNode(){forw = back = NULL;}
+    ListNode(const T &dataIn, ListNode *forw = NULL, ListNode *back = NULL){ data = dataIn;}
+    
+    // setters
+
+    // getters
+
+};
+
+
+#endif
