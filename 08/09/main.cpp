a5103c5de8f6f1ddc1314b8ca2db8b0556d0462b
NoahCardoza <noahcardoza@gmail.com>
2021-10-22 10:24:40 -0700

    lint 8.9

--- 08/09/main.cpp
+++ 08/09/main.cpp
@@ -1,4 +1,4 @@
-#include "BinarySearchTree.h"  // BST ADT
+#include "BinarySearchTree.h" // BST ADT
 #include <iostream>
 #include <string>
 
@@ -12,22 +12,22 @@ void iDisplay(int &, int);
 int main()
 {
     BinarySearchTree<int> bst;
-    
+
     int n;
-    
+
     cout << "What is the number of nodes in the BST? " << endl;
     cin >> n;
-    
+
     buildBST(n, bst);
-    
+
     cout << "  Inorder: ";
     bst.inOrder(hDisplay);
     cout << endl;
-    
+
     cout << "Indented Tree:\n";
-    bst.printTree(iDisplay);        
+    bst.printTree(iDisplay);
     return 0;
-}  
+}
 
 /*
  buildBST: builds a binary search tree
@@ -36,10 +36,10 @@ int main()
 void buildBST(int n, BinarySearchTree<int> &bst)
 {
     int item;
-    
-    while(n--)
+
+    while (n--)
     {
-        item = rand( ) % 30 + 10;
+        item = rand() % 30 + 10;
         bst.insert(item);
     }
 }
@@ -47,7 +47,7 @@ void buildBST(int n, BinarySearchTree<int> &bst)
 /*
  horizontal display: all items on one line
 */
-void hDisplay (int &item)
+void hDisplay(int &item)
 {
     cout << item << " ";
 }
@@ -55,7 +55,7 @@ void hDisplay (int &item)
 /*
  vertical display: one item per line
 */
-void vDisplay (int &item)
+void vDisplay(int &item)
 {
     cout << item << endl;
 }
@@ -68,5 +68,4 @@ void iDisplay(int &item, int level)
     for (int i = 1; i < level; i++)
         cout << "..";
     cout << level << "). " << item << endl;
-    
 }
