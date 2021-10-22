888fea238c3e2563cba7d832632b191c94244f27
NoahCardoza <noahcardoza@gmail.com>
2021-10-22 10:51:40 -0700

    start 8.14

---
+++ 08/14/College.h
@@ -0,0 +1,45 @@
+// Specification file for the College class
+// Written By: A. Student
+// Changed By:
+// IDE: Xcode
+
+#ifndef COLLEGE_H
+#define COLLEGE_H
+
+  
+#include<string>
+
+using std::string;
+using std::ostream;
+
+class College; // Forward Declaration
+
+// Function Prototypes for Overloaded Stream Operators
+ostream &operator << (ostream &, const College &);
+
+class College
+{
+    private:
+        /* Write your code here */
+
+    public:
+        //constructors
+        /* Write your code here */
+
+        //setters
+         /* Write your code here */
+
+        //getters
+        /* Write your code here */
+
+        //other functions if any
+        /* Write your code here */
+    
+        // overloaded operators
+         /* Write your code here */
+      
+        // Friends
+        friend ostream &operator << (ostream &, const College &);
+};
+
+#endif
