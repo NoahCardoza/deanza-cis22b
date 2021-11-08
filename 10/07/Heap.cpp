b0158b924b01955e5483bf7707159b4303c71d14
NoahCardoza <noahcardoza@gmail.com>
2021-11-07 22:51:34 -0800

    solution to 10/07

--- 10/07/Heap.cpp
+++ 10/07/Heap.cpp
@@ -1,21 +1,37 @@
 /* *~*~*
 Implementation file for the Heap class: min-heap of integers
 Written By: A. Student
-Changed by:
-IDE:  
+Changed by: Noah Cardoza
+IDE: VS Code
 *~**/
 
 #include "Heap.h"
+
+/**
+ * Swaps two variables
+ */
+template <typename T>
+void swap(T *a, T *b)
+{
+	T tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
 
 /* *~*~*
  The private member function _reHeapUp rearranges the heap after insert by moving the
  last item up to the correct location in the heap
  *~**/
-void Heap::_reHeapUp(int lastndx)
+void Heap::_reHeapUp(int index)
 {
-	if (lastndx) // means lastndx != 0, i.e. newElement is not heap's root
+	if (index) // means lastndx != 0, i.e. newElement is not heap's root
 	{
-		/* Write your code here */
+		int parent = _findParent(index);
+		if (heapAry[parent] > heapAry[index])
+		{
+			swap(heapAry + parent, heapAry + index);
+			_reHeapUp(parent);
+		}
 	}
 }
 
@@ -23,12 +39,22 @@ void Heap::_reHeapUp(int lastndx)
  The private member function _reHeapDown rearranges the heap after delete by moving the
  data in the root down to the correct location in the heap
  *~**/
-void Heap::_reHeapDown(int rootdex)
+void Heap::_reHeapDown(int index)
 {
-	int left = _findLeftChild(rootdex);
-	if (left != -1) // if there's a left child
+	int right = _findRightChild(index);
+	int left = _findLeftChild(index);
+
+	int direction =
+		left == -1
+			? right
+			: heapAry[left] < heapAry[right]
+				? left
+				: right;
+
+	if (direction != -1 && heapAry[index] > heapAry[direction])
 	{
-		/* Write your code here */
+		swap(heapAry + index, heapAry + direction);
+		return _reHeapDown(direction);
 	}
 }
 /* *~*~*
@@ -40,7 +66,9 @@ bool Heap::insertHeap(int newItem)
 	if (isFull())
 		return false;
 
-	/* Write your code here */
+	heapAry[count] = newItem;
+
+	_reHeapUp(count++);
 
 	return true;
 }
@@ -54,6 +82,10 @@ bool Heap::deleteHeap(int &returnItem)
 	if (isEmpty())
 		return false;
 
-	/* Write your code here */
+	returnItem = heapAry[0];
+	heapAry[0] = heapAry[--count];
+
+	_reHeapDown(0);
+
 	return true;
 }
